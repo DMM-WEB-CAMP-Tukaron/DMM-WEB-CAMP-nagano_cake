5074be62da9274601c6767f021ee92554b222839
EC2 Default User <ec2-user@ip-172-31-57-107.ec2.internal>
2021-02-20 07:57:16 +0000

    oder,home

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,6 +1,7 @@
 class Public::HomesController < ApplicationController
 
   def top
+    @items = Item.all.limit(5)
   end
 
   def about
--- app/models/addre.rb
+++ app/models/addre.rb
@@ -1,5 +1,9 @@
 class Addre < ApplicationRecord
 
   belongs_to :customer
-  
+
+  def address_select
+    self.address + " " + "〒" + self.postal_code + " " + self.name
+  end
+
 end
