a12a0a0bc89b40a9abba41b656e1405290e50e8e
Daisuke-Sato-0530 <daisuke19940530@gmail.com>
2021-02-20 04:34:16 +0000

    oder model enum書いた

--- app/models/oder.rb
+++ app/models/oder.rb
@@ -1,7 +1,8 @@
 class Oder < ApplicationRecord  
   
   enum billing_amount:    [ :クレジットカード, :銀行振り込み ]
-
+  enum status: [ :入金待ち, :入金確認, :製作中, :発送準備中, :発送済み]
+ 
   belongs_to :customer
   has_many :oder_items, dependent: :destroy
 
