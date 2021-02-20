e3d7ad6f1527b08003420d546d28b307c214b8bb
Daisuke-Sato-0530 <daisuke19940530@gmail.com>
2021-02-20 07:56:09 +0000

    会員編集ページ機能だけ

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,7 +1,31 @@
 class Admin::CustomersController < ApplicationController
 
-  def top
-    @oder_items = Orderitems.where(created_at:  Time.zone.now.all_day)
-  end
+  # def top
+    # @oder_items = Orderitems.where(created_at:  Time.zone.now.all_day)
+  # end
 
   
+  def index
+    @customers = Customer.all
+  end 
+  
+  def show
+    @customer = Customer.find(params[:id])
+  end 
+  
+  def edit
+    @customer = Customer.find(params[:id])
+  end 
+  
+  def update 
+    @customer = Customer.find(params[:id])
+      @customer.update(customer_params)
+        redirect_to admin_customer_path
+
+  end 
+  
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email, :is_deleted)
+  end
+  
+end 
