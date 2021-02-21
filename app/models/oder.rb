eaeb9009ce76ab6b39670c09942bd6de82abb94e
Daisuke-Sato-0530 <daisuke19940530@gmail.com>
2021-02-21 13:47:05 +0000

    注文履歴追記

--- app/models/oder.rb
+++ app/models/oder.rb
@@ -2,6 +2,7 @@ class Oder < ApplicationRecord
 
   # 支払い方法選択
   enum billing_amount: { "クレジットカード": 0, "銀行振り込み": 1 }
+  enum status: { "入金待ち":0, "入金確認":1, "製作中":2, "発送準備中":3, "発送済み":4}
 
   belongs_to :customer
   has_many :oder_items, dependent: :destroy
