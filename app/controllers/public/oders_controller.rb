f5483a126b87665060d3e5098cd82453beb18183
EC2 Default User <ec2-user@ip-172-31-57-107.ec2.internal>
2021-02-21 05:08:31 +0000

    oders,new,comfirm,comlete

--- app/controllers/public/oders_controller.rb
+++ app/controllers/public/oders_controller.rb
@@ -10,27 +10,29 @@ class Public::OdersController < ApplicationController
     @oders = Oder.all
     @oder = Oder.new
     @addre = Addre.all
-
   end
 
   def create
     @oder = Oder.new(oder_params)
     @oder.save
-
+    if  params[:oder][:oder_address_type] == "0"
+    elsif params[:oder][:oder_address_type] == "1"
+    elsif params[:oder][:oder_address_type] == "2"
+    end
     redirect_to complete_path
-  end
-
-  def edit
   end
 
   def comfirm
     @customer = current_customer
-    @oder = Oder.new
+    @oder = Oder.new(oder_params)
     @cart_items = CartItem.all
     @oder_item = OderItem.all
-    if  params[:order][:address_option] == "0"
-    elsif params[:order][:address_option] == "1"
-    elsif params[:order][:address_option] == "2"
+
+    case params[:oder_address_type]
+    when "0"
+      @oder.delivery_postal = current_customer.postal_code #郵便番号
+      @oder.postal_address = current_customer.address #住所
+      @oder.delivery_name = current_customer.last_name + current_customer.first_name #名前
     end
   end
 
@@ -38,5 +40,6 @@ class Public::OdersController < ApplicationController
   def oder_params
     params.require(:oder).permit(:customer_id, :delivery_postal, :postal_address, :delivery_name, :shipping, :payment, :billing_amount, :status )
   end
+
 
 end
