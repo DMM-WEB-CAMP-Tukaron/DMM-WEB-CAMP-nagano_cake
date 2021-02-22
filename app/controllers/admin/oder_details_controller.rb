359f00a62c1d852b9077a04adfb8cc982c03c933
Daisuke-Sato-0530 <daisuke19940530@gmail.com>
2021-02-22 05:42:50 +0000

    注文履歴作成

--- app/controllers/admin/oder_details_controller.rb
+++ app/controllers/admin/oder_details_controller.rb
@@ -1,2 +1,15 @@
 class Admin::OderDetailsController < ApplicationController
+
+  def update
+    item = OderItem.find(params[:id])
+    if item.update(item_params)
+      redirect_to admin_oder_path(item.oder_id)
+    end
+  end
+
+
+  private
+    def item_params
+      params.require(:oder_item).permit(:making_status)
+    end
 end
--- app/models/oder_item.rb
+++ app/models/oder_item.rb
@@ -3,4 +3,5 @@ class OderItem < ApplicationRecord
    belongs_to :item
    belongs_to :oder
 
+   enum making_status: {"まだ":0, "作ってる":1, "作り終わった":3}
 end
