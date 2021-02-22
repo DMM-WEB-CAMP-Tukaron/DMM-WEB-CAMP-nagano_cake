5c614edcc8026d2dfa7421b15bd4449f90f9344d
Daisuke-Sato-0530 <daisuke19940530@gmail.com>
2021-02-22 11:29:00 +0000

    admin機能は完成

--- app/models/oder_item.rb
+++ app/models/oder_item.rb
@@ -3,5 +3,5 @@ class OderItem < ApplicationRecord
    belongs_to :item
    belongs_to :oder
 
-   enum making_status: {"まだ":0, "作ってる":1, "作り終わった":3}
+   enum making_status: {"着手不可":0, "制作待ち":1, "製作中":2, "制作完了":3}
 end
