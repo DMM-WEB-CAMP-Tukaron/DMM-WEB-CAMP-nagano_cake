9836f907689cd3aeaa1cc2678acdb9e0be561d98
hidekaxxx <im.hideka.002@gmail.com>
2021-02-23 04:48:50 +0000

    ログイン画面、TOP編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,28 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # ログイン前に遷移するpathを設定
   def after_sign_in_path_for(resource)
-  case resource
-  when Admin
-    admin_customers_path
-  when Customer
-    root_path
+    case resource
+    when Admin
+      admin_homes_top_path
+    when Customer
+      root_path
+    end
   end
-  end
-
+  
+  # ログアウト後に遷移するpathを設定
   def after_sign_out_path_for(resource)
-    root_path # ログアウト後に遷移するpathを設定
+    case resource
+    when Admin
+      new_admin_session_path
+    when Customer
+      root_path
+    end
   end
 
   protected
-
+  # デバイスのストロングパラメータ
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number])
   end
