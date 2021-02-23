a20549e1932f42ed04d648cca70a66c1e7345753
Daisuke-Sato-0530 <daisuke19940530@gmail.com>
2021-02-23 07:33:36 +0000

    pagenate追加 admin/itemsのview微調整

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,28 +1,29 @@
 class Admin::CustomersController < ApplicationController
 
+
   def index
-    @customers = Customer.page(params[:page]).per(3)
-  end 
-  
+    @customers = Customer.page(params[:page]).per(10)
+  end
+
   def show
     @customer = Customer.find(params[:id])
-  end 
-  
+  end
+
   def edit
     @customer = Customer.find(params[:id])
-  end 
-  
-  def update 
+  end
+
+  def update
     @customer = Customer.find(params[:id])
     if  @customer.update(customer_params)
         redirect_to admin_customer_path(@customer)
     else
       render :edit
     end
-  end 
-  
+  end
+
   def customer_params
     params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email, :is_deleted)
   end
-  
-end 
+
+end
